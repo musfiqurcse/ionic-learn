f243cf4850bbd42ea59cf3898620ee0dafd1cb74
musfiqurcse <md.musfiqurcse@outlook.com>
2019-07-21 23:37:34 +0600

    two way data binding done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  foo = 'My hello world';
+  bar = 'Hello My World';
+  firstNumber=  '0';
+  secondNumber= '0';
+  result_item = 0;
+  sumItem = 0;
   constructor(public navCtrl: NavController) {
 
   }
 
+  getText(){
+    let emptyString ='';
+    for(let i=0; i<7; i++){
+      emptyString +=this.bar+' ';
+    }
+    return emptyString;
+  }
+  calculate(){
+    return ++this.result_item;
+  }
+  findSum(){
+    this.sumItem = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+  }
 }
